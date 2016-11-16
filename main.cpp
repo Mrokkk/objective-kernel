61e7b7f86b37be4726f132a46e84920754ab1e35
Mrokkk <mrokk@protonmail.com>
2016-11-16 23:00:01 +0100

    Configure CI

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <linkage.h>
 #include <vga.h>
+#include <reboot.h>
 
 #define __noreturn __attribute__((noreturn))
 
 asmlinkage __noreturn void main() {
     video_init();
     display_print("Hello World!");
+#ifdef CI
+    reboot();
+#endif
     while (1);
 }
 
