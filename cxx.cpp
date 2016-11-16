4f9475c217c117302e69e88e5f0c4323e29bfa75
Mrokkk <mrokk@protonmail.com>
2016-11-16 22:29:45 +0100

    Change constructors.cpp into cxx.cpp and add some C++ symbols

--- cxx.cpp
+++ cxx.cpp
@@ -7,17 +7,22 @@ extern void (*__init_array_end)();
 extern void (*__preinit_array_start)();
 extern void (*__preinit_array_end)();
 
-void _init()
-{
+void * __dso_handle = 0;
+
+int __cxa_atexit(void (*)(void *), void *, void *) {
+    return 0;
+}
+
+void __cxa_pure_virtual() {}
+
+void _init() {
     void (**preinit_constructor)() = &__preinit_array_start;
     void (**init_constructor)() = &__init_array_start;
-
     while (preinit_constructor != &__preinit_array_end)
     {
         (*preinit_constructor)();
         ++preinit_constructor;
     }
-
     while (init_constructor != &__init_array_end)
     {
         (*init_constructor)();
@@ -26,3 +31,4 @@ void _init()
 }
 
 }
+
