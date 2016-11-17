b3071a4ee1fca142656563dbf6e768541a041d3a
Mrokkk <mrokk@protonmail.com>
2016-11-17 22:27:24 +0100

    Small improvements

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -9,22 +9,37 @@ asmlinkage char _end[];
 
 int printf(const char *fmt, ...);
 
+#define assert_eq(v1, v2) \
+    { \
+        if ((v1) != (v2)) printf("assertion failed: line %d: %d != %d\n", __LINE__, v1, v2); \
+    }
+
+#define assert_neq(v1, v2) \
+    { \
+        if ((v1) == (v2)) printf("assertion failed: line %d: %d != %d\n", __LINE__, v1, v2); \
+    }
+
 asmlinkage __noreturn void main() {
     video_init();
     printf("Hello World!\n");
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 1024; i++) {
         auto a = yacppl::make_shared<int>(1);
+        assert_eq(*a, 1);
         auto b = yacppl::make_unique<char>(2);
+        assert_eq(*b, 2);
+        assert_neq(reinterpret_cast<unsigned int>(b.get()), reinterpret_cast<unsigned int>(a.get()));
         auto c = new int{498};
-        printf("a = %d [0x%x]\n", *a, reinterpret_cast<unsigned int>(a.get()));
-        printf("b = %d [0x%x]\n", *b, reinterpret_cast<unsigned int>(b.get()));
-        printf("c = %d [0x%x]\n", *c, reinterpret_cast<unsigned int>(c));
+        assert_eq(*c, 498);
+        assert_neq(reinterpret_cast<unsigned int>(c), reinterpret_cast<unsigned int>(a.get()));
+        assert_neq(reinterpret_cast<unsigned int>(c), reinterpret_cast<unsigned int>(b.get()));
+        assert_eq(*a, 1);
+        assert_eq(*b, 2);
         delete c;
         auto d = new int(5);
-        printf("c = %d [0x%x]\n", *c, reinterpret_cast<unsigned int>(c));
-        printf("d = %d [0x%x]\n", *d, reinterpret_cast<unsigned int>(d));
         delete d;
+        assert_eq(c, d);
     }
+    printf("all tests passed!");
 #ifdef CI
     reboot();
 #endif
