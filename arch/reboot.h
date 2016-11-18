fc2ac3598294dd0db3cb164b2a442f6ba85f3d51
Mrokkk <mrokk@protonmail.com>
2016-11-18 21:57:46 +0100

    Change the way of rebooting

--- arch/reboot.h
+++ arch/reboot.h
@@ -5,12 +5,17 @@
 #define halt() \
     asm volatile("hlt")
 
+static inline void gdt_load(void *gdt) {
+    asm volatile(
+        "    lgdt (%%eax);"
+        "    ljmp $0x08, $1f;"
+        "1:"
+        :: "a" (gdt)
+    );
+}
+
 inline void reboot() {
-    unsigned char dummy = 0x02;
-    while (dummy & 0x02)
-        dummy = inb(0x64);
-    outb(0xfe, 0x64);
-    halt();
+    gdt_load(nullptr);
     while (1);
 }
 
