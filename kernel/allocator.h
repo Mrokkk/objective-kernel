f03fab33a0f30900cb6e7c0a3dacdd4d37b9ec9c
Mrokkk <mrokk@protonmail.com>
2016-11-23 21:52:03 +0100

    allocator.h: refactoring

--- kernel/allocator.h
+++ kernel/allocator.h
@@ -10,25 +10,38 @@ namespace kernel {
 template <class Heap_Allocator, size_t _memory_block_size>
 class allocator final {
 
-    struct memory_block {
-        size_t size;
-        bool free;
-        yacppl::af_list<memory_block> blocks;
-        explicit memory_block(size_t s)
-            : size(s), free(false), blocks(&memory_block::blocks) {
-        }
-    };
 
     struct memory_block_data {
         unsigned char dummy[_memory_block_size];
         unsigned int block_ptr[0];
     } __packed;
 
+    struct memory_block {
+
+        size_t size;
+        bool free;
+        yacppl::af_list<memory_block> blocks;
+
+        explicit memory_block(size_t s)
+            : size(s), free(false), blocks(&memory_block::blocks) {
+        }
+
+        void *data() {
+            return reinterpret_cast<memory_block_data *>(this)->block_ptr;
+        }
+
+    };
+
     yacppl::af_list<memory_block> _blocks;
     Heap_Allocator _heap_allocator;
 
     memory_block *create_memory_block(size_t size) {
         return new(_heap_allocator.grow_heap(_memory_block_size + size)) memory_block(size);
+    }
+
+    template <typename Ptr>
+    Ptr pointer_offset(Ptr ptr, int off) {
+        return reinterpret_cast<Ptr>(reinterpret_cast<char *>(ptr) + off);
     }
 
 public:
@@ -48,22 +61,22 @@ public:
                 }
                 else {
                     auto block = new(&temp) memory_block(size);
-                    new_block = reinterpret_cast<memory_block *>(reinterpret_cast<size_t>(reinterpret_cast<memory_block_data *>(block)->block_ptr) + size);
+                    new_block = reinterpret_cast<memory_block *>(pointer_offset(block->data(), size));
                     new_block->size = old_size - _memory_block_size - temp.size;
                     new_block->free = true;
                     temp.blocks.add_front(&new_block->blocks);
                 }
-                return static_cast<void *>(reinterpret_cast<memory_block_data *>(&temp)->block_ptr);
+                return temp.data();
             }
         }
         if (!(new_block = create_memory_block(size))) return 0;
         _blocks.add(&new_block->blocks);
-        return static_cast<void *>(reinterpret_cast<memory_block_data *>(new_block)->block_ptr);
+        return new_block->data();
     }
 
     int free(void *address) {
         for (auto &temp : _blocks) {
-            if (reinterpret_cast<unsigned long>(reinterpret_cast<memory_block_data *>(&temp)->block_ptr) == reinterpret_cast<unsigned long>(address)) {
+            if (temp.data() == address) {
                 temp.free = true;
                 return 0;
             }
