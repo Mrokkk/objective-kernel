e22105d024f77c014b16c5d318d1526451a669bb
Mrokkk <mrokk@protonmail.com>
2016-11-25 11:18:23 +0100

    Add support for Multiboot2, boot kernel only from iso

--- CMake/isomaker.sh
+++ CMake/isomaker.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
+set -e
+
+base_dir=$(dirname $0)
 name=$1
+multiboot2=$2
+
 mkdir -p ${name}.d/boot/grub
 
-if [ ! -f ${name}.d/boot/grub/menu.lst ]; then
-    echo default 0 > ${name}.d/boot/grub/menu.lst
-    echo timeout 0 >> ${name}.d/boot/grub/menu.lst
-    echo title My kernel >> ${name}.d/boot/grub/menu.lst
-    echo kernel /kernel >> ${name}.d/boot/grub/menu.lst
-    echo boot >> ${name}.d/boot/grub/menu.lst
+if [ "$multiboot2" == "--multiboot2" ]; then
+    if [ ! -f ${name}.d/boot/grub/grub.cfg ]; then
+        echo "set timeout=0
+set default=0
+
+menuentry "${name}" {
+    multiboot2 /kernel
+    boot
+}" > ${name}.d/boot/grub/grub.cfg
+    fi
+    cp ${name} ${name}.d/kernel
+    grub-mkrescue -o ${name}.iso ${name}.d
+else
+    if [ ! -f ${name}.d/boot/grub/menu.lst ]; then
+        echo default 0 > ${name}.d/boot/grub/menu.lst
+        echo timeout 0 >> ${name}.d/boot/grub/menu.lst
+        echo title My kernel >> ${name}.d/boot/grub/menu.lst
+        echo kernel /kernel >> ${name}.d/boot/grub/menu.lst
+        echo boot >> ${name}.d/boot/grub/menu.lst
+    fi
+
+    cp $base_dir/stage2_eltorito ${name}.d/boot/grub/stage2_eltorito
+
+    cp ${name} ${name}.d/kernel
+    $(which genisoimage || which mkisofs) -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o ${name}.iso ${name}.d 2> /dev/null
 fi
-
-if [ ! -f stage2_eltorito ]; then
-    echo Downloading bootloader stage2...
-    wget -q ftp://ftp.free.org/mirrors/rsync.frugalware.org/frugalware-testing/boot/grub/stage2_eltorito
-fi
-
-cp stage2_eltorito ${name}.d/boot/grub/stage2_eltorito
-
-cp ${name} ${name}.d/kernel
-$(which genisoimage || which mkisofs) -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o ${name}.iso ${name}.d 2> /dev/null
-
