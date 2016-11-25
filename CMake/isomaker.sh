14fa90f8679362149fd9e1c81c3b44c5ab41b46b
Mrokkk <mrokk@protonmail.com>
2016-11-25 12:33:05 +0100

    isomaker.sh: improvements

--- CMake/isomaker.sh
+++ CMake/isomaker.sh
@@ -12,7 +12,6 @@ if [ "$multiboot2" == "--multiboot2" ]; then
     if [ ! -f ${name}.d/boot/grub/grub.cfg ]; then
         echo "set timeout=0
 set default=0
-
 menuentry "${name}" {
     multiboot2 /kernel
     boot
@@ -22,15 +21,14 @@ menuentry "${name}" {
     grub-mkrescue -o ${name}.iso ${name}.d
 else
     if [ ! -f ${name}.d/boot/grub/menu.lst ]; then
-        echo default 0 > ${name}.d/boot/grub/menu.lst
-        echo timeout 0 >> ${name}.d/boot/grub/menu.lst
-        echo title My kernel >> ${name}.d/boot/grub/menu.lst
-        echo kernel /kernel >> ${name}.d/boot/grub/menu.lst
-        echo boot >> ${name}.d/boot/grub/menu.lst
+        echo "default 0
+timeout 0
+title My kernel
+kernel /kernel
+boot" > ${name}.d/boot/grub/menu.lst
     fi
-
     cp $base_dir/stage2_eltorito ${name}.d/boot/grub/stage2_eltorito
-
     cp ${name} ${name}.d/kernel
     $(which genisoimage || which mkisofs) -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o ${name}.iso ${name}.d 2> /dev/null
 fi
+
