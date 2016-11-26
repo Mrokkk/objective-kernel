efc34be5f319594888d4c825a6a95b34f69cc2f8
Mrokkk <mrokk@protonmail.com>
2016-11-26 16:14:55 +0100

    Use inherited_list instead of kernel_list

--- kernel/allocator.h
+++ kernel/allocator.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <kernel_list.h>
+#include <inherited_list.h>
 #include <basic/new.h>
 
 namespace kernel {
@@ -8,7 +8,7 @@ namespace kernel {
 template <class Heap_Allocator, size_t _memory_block_size>
 class allocator final {
 
-    class memory_block {
+    class memory_block : public yacppl::inherited_list<memory_block> {
 
         struct _data {
             unsigned char data[_memory_block_size];
@@ -24,10 +24,9 @@ class allocator final {
 
         size_t size;
         bool free = false;
-        yacppl::kernel_list<memory_block> blocks;
 
         explicit memory_block(size_t s)
-            : size(s), free(false), blocks(&memory_block::blocks) {
+            : size(s), free(false) {
         }
 
         void divide(size_t pivot) {
@@ -37,7 +36,7 @@ class allocator final {
             auto new_block = reinterpret_cast<memory_block *>(pointer_offset(data(), pivot));
             new_block->size = old_size - _memory_block_size - size;
             new_block->free = true;
-            blocks.add_front(&new_block->blocks);
+            yacppl::inherited_list<memory_block>::add_front(new_block);
         }
 
         void try_to_divide(size_t pivot) {
@@ -55,7 +54,7 @@ class allocator final {
 
     };
 
-    yacppl::kernel_list<memory_block> _blocks;
+    yacppl::inherited_list<memory_block> _blocks;
     Heap_Allocator _heap_allocator;
 
     void adapt_size(size_t &size) {
@@ -70,7 +69,7 @@ class allocator final {
 public:
 
     explicit allocator(char *heap_start)
-        : _blocks(&memory_block::blocks), _heap_allocator(heap_start) {}
+        : _heap_allocator(heap_start) {}
 
     void *allocate(size_t size) {
         adapt_size(size);
@@ -82,7 +81,7 @@ public:
         }
         auto new_block = create_memory_block(size);
         if (new_block == nullptr) return 0;
-        _blocks.add(&new_block->blocks);
+        _blocks.add(new_block);
         return new_block->data();
     }
 
@@ -92,11 +91,11 @@ public:
                 temp.free = true;
                 return 0;
             }
-            auto next = temp.blocks.next_entry();
+            auto next = temp.next();
             if (!next) continue;
             if (next->free && temp.free) {
                 temp.size = temp.size + next->size + _memory_block_size;
-                next->blocks.remove();
+                next->remove();
             }
         }
         return -1;
