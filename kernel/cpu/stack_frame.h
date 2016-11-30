3128e416c6fff9a353a913f09d94b0ccc342e8ee
Mrokkk <mrokk@protonmail.com>
2016-11-30 10:26:04 +0100

    Add structure for stack frame

---
+++ kernel/cpu/stack_frame.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace cpu {
+
+struct stack_frame {
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t ebp;
+    uint32_t eax;
+    uint16_t ds, __ds;
+    uint16_t es, __es;
+    uint16_t fs, __fs;
+    uint16_t gs, __gs;
+    uint32_t eip;
+    uint16_t cs, __cs;
+    uint32_t eflags;
+    uint32_t esp;
+    uint16_t ss, __ss;
+};
+
+} // namespace cpu
+
