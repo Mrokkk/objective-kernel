6a3b8edd7b73520d625b6a920371d0981e4a16c9
Mrokkk <mrokk@protonmail.com>
2016-11-30 10:35:44 +0100

    Add function parameters to the exception_handler

--- kernel/cpu/exceptions.cpp
+++ kernel/cpu/exceptions.cpp
@@ -1,5 +1,7 @@
 #include <kernel/cpu/exceptions.h>
+#include <kernel/cpu/stack_frame.h>
 
-asmlinkage void exception_handler() {
+asmlinkage void exception_handler(uint32_t, uint32_t, cpu::stack_frame) {
+
 }
 
