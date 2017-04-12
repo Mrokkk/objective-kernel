3f0e10743b5072d09f751e1e953ae502805f698e
Mrokkk <mrokk@protonmail.com>
2017-04-12 21:06:41 +0200

    Fix tests order

--- scripts/create_test_images.sh
+++ scripts/create_test_images.sh
@@ -11,7 +11,9 @@ IFS=$'\n'
 tests=(`strings ${name} | grep test_cases | c++filt | grep "()" | sed 's/test_cases:://g;s/__/\./g;s/()//g'`)
 unset IFS
 
+index=0
 for t in "${tests[@]}"; do
-    ${BASEDIR}/isomaker.sh -i ${name} -o "${t}.iso" ${multiboot_flag} --grub-use-serial --args "${t}"
+    ${BASEDIR}/isomaker.sh -i ${name} -o "${index}-${t}.iso" ${multiboot_flag} --grub-use-serial --args "${t}"
+    ((index+=1))
 done
 
