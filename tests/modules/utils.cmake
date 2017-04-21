7012b85cd5c3eacd467a2e2402a06ca5ff86de07
Mrokkk <mrokk@protonmail.com>
2017-04-21 12:24:33 +0200

    Refactoring

--- tests/modules/utils.cmake
+++ tests/modules/utils.cmake
@@ -16,9 +16,3 @@ function(add_valgrind_target executable target-name)
     )
 endfunction(add_valgrind_target)
 
-function(add_sanitizers_flags)
-    set(SANITIZERS_FLAGS "-fsanitize=address,undefined")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${SANITIZERS_FLAGS}" PARENT_SCOPE)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SANITIZERS_FLAGS}")
-endfunction(add_sanitizers_flags)
-
