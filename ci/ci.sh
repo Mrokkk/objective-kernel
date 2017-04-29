fbd0d4cd9936fa42c2002d6a10ef6fede44a1d89
Mrokkk <mrokk@protonmail.com>
2017-04-29 17:03:17 +0200

    Fix sanitizers job

--- ci/ci.sh
+++ ci/ci.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker run -t -v $PWD:$PWD $IMAGE /bin/bash -c "OPTIMIZE=$OPTIMIZE MULTIBOOT2=$MULTIBOOT2 CXX=$COMPILER UT=$UT $PWD/ci/run_tests.sh"
+docker run -t -v $PWD:$PWD $IMAGE /bin/bash -c "OPTIMIZE=$OPTIMIZE MULTIBOOT2=$MULTIBOOT2 CXX=$COMPILER UT=$UT SANITIZERS=$SANITIZERS $PWD/ci/run_tests.sh"
 
