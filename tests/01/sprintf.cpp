5f8384b4c453444a5720821be23016386c6639ae
Mrokkk <mrokk@protonmail.com>
2017-04-29 20:28:14 +0200

    Next tests

--- tests/01/sprintf.cpp
+++ tests/01/sprintf.cpp
@@ -6,6 +6,12 @@ namespace {
 
 } // namespace
 
+TEST(sprintf, can_format_percent_sign) {
+    char buffer[32];
+    sprintf(buffer, "%%");
+    REQUIRE_EQ((const char *)buffer, "%");
+}
+
 TEST(sprintf, can_format_int) {
     char buffer[32];
     sprintf(buffer, "%d", 21);
@@ -14,6 +20,8 @@ TEST(sprintf, can_format_int) {
     REQUIRE_EQ((const char *)buffer, "-910");
     sprintf(buffer, "%d", 0);
     REQUIRE_EQ((const char *)buffer, "0");
+    sprintf(buffer, "%+d", 124);
+    REQUIRE_EQ((const char *)buffer, "+124");
 }
 
 TEST(sprintf, can_format_unsigned_int) {
@@ -44,11 +52,25 @@ TEST(sprintf, can_format_string) {
     REQUIRE_EQ((const char *)buffer, "testing");
 }
 
+TEST(sprintf, can_format_pointer) {
+    char buffer[32];
+    sprintf(buffer, "%p", 0x21);
+    REQUIRE_EQ((const char *)buffer, "0x0000000000000021");
+}
+
 TEST(sprintf, can_pad) {
     char buffer[32];
     sprintf(buffer, "%06d", 324);
     REQUIRE_EQ((const char *)buffer, "000324");
-    sprintf(buffer, "0x%03x", 0x21);
+    sprintf(buffer, "%#05x", 0x21);
     REQUIRE_EQ((const char *)buffer, "0x021");
+    sprintf(buffer, "%6d", 324);
+    REQUIRE_EQ((const char *)buffer, "   324");
+    sprintf(buffer, "%6c", 'c');
+    REQUIRE_EQ((const char *)buffer, "     c");
+    sprintf(buffer, "%-6c", 'c');
+    REQUIRE_EQ((const char *)buffer, "c     ");
+    sprintf(buffer, "% 6c", 'c');
+    REQUIRE_EQ((const char *)buffer, "     c");
 }
 
