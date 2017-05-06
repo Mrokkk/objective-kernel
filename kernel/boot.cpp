869a5a4406a2ac799490492acd07e75fceddbbd9
Mrokkk <mrokk@protonmail.com>
2017-05-06 22:21:31 +0200

    Move functions from boot.cpp to text.boot section

--- kernel/boot.cpp
+++ kernel/boot.cpp
@@ -10,6 +10,7 @@ namespace boot {
 char cmdline[128];
 uint32_t upper_mem = 0u;
 
+__section(.text.boot)
 char *get_mb2_cmdline(mb2_tags_header *tag) {
     for (auto temp = reinterpret_cast<mb2_tag *>(memory::virt_address(tag) + 1); temp->type != 0;
             temp = reinterpret_cast<mb2_tag *>(((char *)temp + ((temp->size + 7) & ~7)))) {
@@ -20,7 +21,7 @@ char *get_mb2_cmdline(mb2_tags_header *tag) {
     return nullptr;
 }
 
-
+__section(.text.boot)
 uint32_t get_mb2_upper_mem(mb2_tags_header *tag) {
     for (auto temp = reinterpret_cast<mb2_tag *>(memory::virt_address(tag) + 1); temp->type != 0;
             temp = reinterpret_cast<mb2_tag *>(((uint8_t *)temp + ((temp->size + 7) & ~7)))) {
@@ -31,6 +32,7 @@ uint32_t get_mb2_upper_mem(mb2_tags_header *tag) {
     return 0;
 }
 
+__section(.text.boot)
 uint32_t read_upper_mem(void *data, uint32_t magic) {
     if (magic == MULTIBOOT_BOOTLOADER_MAGIC)
         return static_cast<multiboot_info *>(data)->mem_upper;
@@ -39,6 +41,7 @@ uint32_t read_upper_mem(void *data, uint32_t magic) {
     return 0;
 }
 
+__section(.text.boot)
 char *read_cmdline(void *data, uint32_t magic) {
     if (magic == MULTIBOOT_BOOTLOADER_MAGIC)
         return static_cast<multiboot_info *>(data)->cmdline;
@@ -47,7 +50,8 @@ char *read_cmdline(void *data, uint32_t magic) {
     return nullptr;
 }
 
-asmlinkage void read_bootloader_data(void *data, uint32_t magic) {
+asmlinkage __section(.text.boot)
+void read_bootloader_data(void *data, uint32_t magic) {
     auto cmdline_ptr = read_cmdline(data, magic);
     if (cmdline_ptr == nullptr) {
         cmdline[0] = 0;
