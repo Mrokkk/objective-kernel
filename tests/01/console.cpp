b15ef5fa090d564a3617653c0197ac6d952321e7
Mrokkk <mrokk@protonmail.com>
2017-05-07 22:29:00 +0200

    Fix compilation

--- tests/01/console.cpp
+++ tests/01/console.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
 #include <cstdarg>
+namespace yacppl {}
+namespace utils = yacppl;
 int vsprintf(char *buf, const char *fmt, va_list args);
 int sprintf(char *buf, const char *fmt, ...);
 #include <kernel/console/console.hpp>
