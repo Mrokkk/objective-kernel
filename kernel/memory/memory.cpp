c0813626c9fc91d73b427ad2143b78d5be6b39f8
Mrokkk <mrokk@protonmail.com>
2017-05-07 13:11:22 +0200

    Rename allocator instance

--- kernel/memory/memory.cpp
+++ kernel/memory/memory.cpp
@@ -7,13 +7,13 @@ namespace memory {
 
 using allocator = utils::allocator<paging::page_allocator, 32>;
 
-allocator *a = nullptr;
+allocator *kernel_allocator = nullptr;
 char *allocator_memory = nullptr;
 
 void initialize() {
     paging::initialize();
     allocator_memory = (char *)paging::page_alloc();
-    a = new(allocator_memory) allocator(0);
+    kernel_allocator = new(allocator_memory) allocator(0);
 }
 
 } // namespace memory
