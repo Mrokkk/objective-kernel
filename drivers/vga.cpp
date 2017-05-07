c87258dd7271bb26eeb2d1ea4c154fb8d99e7957
Mrokkk <mrokk@protonmail.com>
2017-05-07 22:49:18 +0200

    VGA driver refactoring

--- drivers/vga.cpp
+++ drivers/vga.cpp
@@ -3,8 +3,7 @@
 #include <kernel/cpu/io.hpp>
 #include <kernel/memory/paging/paging.hpp>
 
-#define blink               (1 << 7)
-
+#define blink (1 << 7)
 #define RESX 80
 #define RESY 25
 
@@ -31,8 +30,6 @@ enum class color : uint8_t {
     white
 };
 
-namespace {
-
 constexpr uint8_t forecolor(color c) {
     return static_cast<uint8_t>(c) & 0xf;
 }
@@ -41,61 +38,36 @@ constexpr uint8_t backcolor(color c) {
     return (static_cast<uint8_t>(c) << 4) & 0x7f;
 }
 
-uint16_t *pointer = reinterpret_cast<uint16_t *>(memory::virt_address(0xb8000u));
+struct video_char {
+
+    uint8_t c;
+    uint8_t attr;
+
+    video_char() : c(' '), attr(forecolor(color::gray) | backcolor(color::black)) {
+    }
+
+    video_char(uint8_t c, uint8_t attr) : c(c), attr(attr) {
+    }
+
+} __packed;
+
+video_char *pointer = reinterpret_cast<video_char *>(memory::virt_address(0xb8000u));
 uint8_t default_attribute = forecolor(color::gray) | backcolor(color::black);
-uint8_t csr_x = 0;
-uint8_t csr_y = 0;
-
-void video_mem_write(uint16_t data, uint16_t offset) {
-    pointer[offset] = data;
-}
-
-void cursor_x_set(int x) {
-    csr_x = x;
-}
-
-void cursor_y_set(int y) {
-    csr_y = y;
-}
-
-void cursor_x_inc() {
-    csr_x++;
-}
-
-void cursor_y_inc() {
-    csr_y++;
-}
-
-void cursor_x_dec() {
-    csr_x--;
-}
-
-int cursor_x_get() {
-    return csr_x;
-}
-
-int cursor_y_get() {
-    return csr_y;
-}
-
-constexpr uint16_t make_video_char(char c, char a) {
-    return ((a << 8) | c);
-}
+uint8_t csr_x;
+uint8_t csr_y;
 
 uint16_t current_offset_get() {
-    return cursor_y_get() * RESX + cursor_x_get();
+    return csr_y * RESX + csr_x;
 }
 
-} // namespace
-
 void scroll(void) {
-    auto blank = ' ' | (default_attribute << 8);
-    if(cursor_y_get() >= RESY) {
-        auto temp = static_cast<uint16_t>(cursor_y_get() - RESY + 1);
+    video_char blank(' ', default_attribute << 8);
+    if(csr_y >= RESY) {
+        auto temp = static_cast<uint16_t>(csr_y - RESY + 1);
         utils::memcopy(reinterpret_cast<const char *>(pointer + temp * RESX),
             reinterpret_cast<char *>(pointer), (RESY - temp) * RESX * 2);
         utils::fill(pointer + (RESY - temp) * RESX, RESX, blank);
-        cursor_y_set(RESY - 1);
+        csr_y = RESY - 1;
     }
 }
 
@@ -108,36 +80,36 @@ void move_csr(void) {
 }
 
 void cls() {
-    auto blank = ' ' | (default_attribute << 8);
+    video_char blank(' ', default_attribute << 8);
     utils::fill(pointer, RESX * RESY, blank);
-    cursor_x_set(0);
-    cursor_y_set(0);
+    csr_x = 0u;
+    csr_y = 0u;
     move_csr();
 }
 
 void putch(unsigned char c) {
     if (c == '\b') {
-        if(cursor_x_get() != 0) {
-            cursor_x_dec();
+        if(csr_x != 0) {
+            csr_x--;
             putch(' ');
-            cursor_x_dec();
+            csr_x--;
         }
     } else if (c == '\t') {
-        cursor_x_set((cursor_x_get() + 8) & ~(8 - 1));
+        csr_x = (csr_x + 8) & ~(8 - 1);
     }
     else if (c == '\r') {
-        cursor_x_set(0);
+        csr_x = 0u;
     }
     else if (c == '\n') {
-        cursor_x_set(0);
-        cursor_y_inc();
+        csr_x = 0u;
+        csr_y++;
     } else if (c >= ' ') {
-        video_mem_write(make_video_char(c, default_attribute), current_offset_get());
-        cursor_x_inc();
+        pointer[current_offset_get()] = video_char(c, default_attribute);
+        csr_x++;
     }
     if (csr_x >= RESX) {
-        cursor_x_set(0);
-        cursor_y_inc();
+        csr_x = 0u;
+        csr_y++;
     }
     scroll();
     move_csr();
