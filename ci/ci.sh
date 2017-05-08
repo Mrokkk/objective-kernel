0427eb19d58179cea3bd0c526a03daa117786d37
Mrokkk <mrokk@protonmail.com>
2017-05-08 20:47:37 +0200

    Move CI scripts into scripts directory

--- ci/ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker run -t -v $PWD:$PWD $IMAGE /bin/bash -c "OPTIMIZE=$OPTIMIZE MULTIBOOT2=$MULTIBOOT2 CXX=$COMPILER UT=$UT SANITIZERS=$SANITIZERS $PWD/ci/run_tests.sh"
-
---
+++ scripts/ci/ci.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+docker run -t -v $PWD:$PWD $IMAGE /bin/bash -c "OPTIMIZE=$OPTIMIZE MULTIBOOT2=$MULTIBOOT2 CXX=$COMPILER UT=$UT SANITIZERS=$SANITIZERS $PWD/scripts/ci/run_tests.sh"
+
