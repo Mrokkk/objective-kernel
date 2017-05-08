45bced47fc662495fe2c8bff88ddac8fd604f10f
Mrokkk <mrokk@protonmail.com>
2017-05-08 23:50:23 +0200

    Remove preinit_array; this closes #1

--- kernel/cpp_support.cpp
+++ kernel/cpp_support.cpp
@@ -6,8 +6,6 @@ asmlinkage {
 
 extern void (*__init_array_start)();
 extern void (*__init_array_end)();
-extern void (*__preinit_array_start)();
-extern void (*__preinit_array_end)();
 
 void * __dso_handle = 0;
 
@@ -71,12 +69,7 @@ void operator delete[](void *address, size_t) noexcept {
 namespace cpp_support {
 
 void initialize() {
-    void (**preinit_constructor)() = &__preinit_array_start;
     void (**init_constructor)() = &__init_array_start;
-    while (preinit_constructor != &__preinit_array_end) {
-        (*preinit_constructor)();
-        ++preinit_constructor;
-    }
     while (init_constructor != &__init_array_end) {
         (*init_constructor)();
         ++init_constructor;
