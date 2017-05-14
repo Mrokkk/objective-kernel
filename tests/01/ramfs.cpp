4b92be2ce7f36b42d8bff3382dc400e16544ad7e
Mrokkk <mrokk@protonmail.com>
2017-05-14 23:56:46 +0200

    Use fixture in ramfs tests

--- tests/01/ramfs.cpp
+++ tests/01/ramfs.cpp
@@ -8,38 +8,37 @@
 
 #include <yatf.hpp>
 
-TEST(ramfs, can_create) {
+struct ramfs_fixture {
     ramfs::ramfs fs;
+};
+
+TEST(ramfs, can_create, ramfs_fixture) {
     REQUIRE_EQ((const char *)fs.name(), "ramfs");
     auto root_node = fs.lookup("/", nullptr);
     REQUIRE(root_node);
     REQUIRE_EQ(root_node->id, 1u);
 }
 
-TEST(ramfs, parent_node_has_to_be_passed_to_create) {
-    ramfs::ramfs fs;
+TEST(ramfs, parent_node_has_to_be_passed_to_create, ramfs_fixture) {
     REQUIRE_FALSE(fs.create("file", nullptr, vfs::vnode::type::file));
     REQUIRE_FALSE(fs.create("dir", nullptr, vfs::vnode::type::dir));
 }
 
-TEST(ramfs, can_create_dir) {
-    ramfs::ramfs fs;
+TEST(ramfs, can_create_dir, ramfs_fixture) {
     auto root_node = fs.lookup("/", nullptr);
     auto dir_node = fs.create("dir", root_node, vfs::vnode::type::dir);
     REQUIRE(dir_node);
     REQUIRE(dir_node->node_type == vfs::vnode::type::dir);
 }
 
-TEST(ramfs, can_create_file) {
-    ramfs::ramfs fs;
+TEST(ramfs, can_create_file, ramfs_fixture) {
     auto root_node = fs.lookup("/", nullptr);
     auto dir_node = fs.create("file", root_node, vfs::vnode::type::file);
     REQUIRE(dir_node);
     REQUIRE(dir_node->node_type == vfs::vnode::type::file);
 }
 
-TEST(ramfs, can_lookup_for_file_in_root_dir) {
-    ramfs::ramfs fs;
+TEST(ramfs, can_lookup_for_file_in_root_dir, ramfs_fixture) {
     auto root_node = fs.lookup("/", nullptr);
     REQUIRE_FALSE(fs.lookup("/file1", nullptr));
     fs.create("file1", root_node, vfs::vnode::type::file);
@@ -51,8 +50,7 @@ TEST(ramfs, can_lookup_for_file_in_root_dir) {
     REQUIRE_FALSE(fs.lookup("/file4", nullptr));
 }
 
-TEST(ramfs, can_create_file_in_dir) {
-    ramfs::ramfs fs;
+TEST(ramfs, can_create_file_in_dir, ramfs_fixture) {
     auto root_node = fs.lookup("/", nullptr);
     auto dir_node = fs.create("dir", root_node, vfs::vnode::type::dir);
     auto file_node = fs.create("file", dir_node, vfs::vnode::type::file);
@@ -60,8 +58,7 @@ TEST(ramfs, can_create_file_in_dir) {
     REQUIRE(file_node->node_type == vfs::vnode::type::file);
 }
 
-TEST(ramfs, can_lookup_for_file_in_dir) {
-    ramfs::ramfs fs;
+TEST(ramfs, can_lookup_for_file_in_dir, ramfs_fixture) {
     auto root_node = fs.lookup("/", nullptr);
     auto dir_node = fs.create("dir", root_node, vfs::vnode::type::dir);
     REQUIRE_FALSE(fs.lookup("/dir/file", nullptr));
