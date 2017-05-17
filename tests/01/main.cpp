53bea33f474bf1d65feaa41822f8ad5115be0634
Mrokkk <mrokk@protonmail.com>
2017-05-18 00:02:27 +0200

    Fix builds; update yacppl and yatf

--- tests/01/main.cpp
+++ tests/01/main.cpp
@@ -2,6 +2,14 @@
 #include <yatf.hpp>
 #include <cstdio>
 
+void atomic_increment(void *addr) {
+    asm volatile("lock incl (%0)" :: "r" (addr));
+}
+
+void atomic_decrement(void *addr) {
+    asm volatile("lock decl (%0)" :: "r" (addr));
+}
+
 void console_init();
 
 int main(int argc, const char *argv[]) {
