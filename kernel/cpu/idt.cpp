6daf4b58dd67bd26765ab22d2777ad15b3a617e4
Mrokkk <mrokk@protonmail.com>
2017-05-31 20:16:57 +0200

    Refactoring IDT

--- kernel/cpu/idt.cpp
+++ kernel/cpu/idt.cpp
@@ -5,12 +5,44 @@ namespace cpu {
 
 namespace idt {
 
-idt_entry idt_entries[256];
+namespace {
 
-idtr idt = {
-        sizeof(idt_entry) * 256 - 1,
-        idt_entries
+struct entry {
+
+    uint16_t base_lo;
+    uint16_t sel;
+    uint8_t  always0;
+    uint8_t  flags;
+    uint16_t base_hi;
+
+    void set_gate(uint32_t base, uint16_t selector, uint32_t f) {
+        base_lo = base & 0xFFFF;
+        base_hi = (base >> 16) & 0xFFFF;
+        sel = selector;
+        always0 = 0;
+        flags = f | 0x80;
+    }
+
+} PACKED entries[256];
+
+struct idtr {
+
+    uint16_t limit;
+    entry *base;
+
+    void load() {
+        asm volatile(
+            "lidt (%%eax);"
+            :: "a" (this)
+        );
+    }
+
+} PACKED idt = {
+    sizeof(entries) - 1,
+    entries
 };
+
+} // namespace
 
 asmlinkage void systick_handler();
 asmlinkage void syscall_handler();
@@ -34,19 +66,19 @@ asmlinkage void syscall_handler();
 #undef exception_noerrno
 
 #define timer_isr(x) \
-    idt_entries[x].set_gate(reinterpret_cast<uint32_t>(systick_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
+    entries[x].set_gate(reinterpret_cast<uint32_t>(systick_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
 
 #define pic_isr(x) \
-    idt_entries[x].set_gate(reinterpret_cast<uint32_t>(isr_##x), segment::kernel_cs, gdt::flags::type::trap_gate_32);
+    entries[x].set_gate(reinterpret_cast<uint32_t>(isr_##x), segment::kernel_cs, gdt::flags::type::trap_gate_32);
 
 #define syscall_isr(x) \
-    idt_entries[x].set_gate(reinterpret_cast<uint32_t>(syscall_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32 | gdt::flags::ring3);
+    entries[x].set_gate(reinterpret_cast<uint32_t>(syscall_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32 | gdt::flags::ring3);
 
 #define exception_errno(x) \
-    idt_entries[__NR_##x].set_gate(reinterpret_cast<uint32_t>(exc_##x##_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
+    entries[__NR_##x].set_gate(reinterpret_cast<uint32_t>(exc_##x##_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
 
 #define exception_noerrno(x) \
-    idt_entries[__NR_##x].set_gate(reinterpret_cast<uint32_t>(exc_##x##_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
+    entries[__NR_##x].set_gate(reinterpret_cast<uint32_t>(exc_##x##_handler), segment::kernel_cs, gdt::flags::type::trap_gate_32);
 
 void initialize() {
     #include "irqs.hpp"
--- kernel/cpu/idt.hpp
+++ kernel/cpu/idt.hpp
@@ -4,38 +4,6 @@ namespace cpu {
 
 namespace idt {
 
-struct idt_entry {
-
-    uint16_t base_lo;
-    uint16_t sel;
-    uint8_t  always0;
-    uint8_t  flags;
-    uint16_t base_hi;
-
-    void set_gate(uint32_t base, uint16_t selector, uint32_t f) {
-        base_lo = base & 0xFFFF;
-        base_hi = (base >> 16) & 0xFFFF;
-        sel = selector;
-        always0 = 0;
-        flags = f | 0x80;
-    }
-
-} PACKED;
-
-struct idtr {
-
-    uint16_t limit;
-    idt_entry *base;
-
-    void load() {
-        asm volatile(
-            "lidt (%%eax);"
-            :: "a" (this)
-        );
-    }
-
-} PACKED;
-
 void initialize();
 
 } // namespace idt
