7d8ada0a12b1c47baa64c5b0fab69e66cf783158
Mrokkk <mrokk@protonmail.com>
2017-06-07 10:32:50 +0200

    Move character::read and ::write method definitions to the .cpp

---
+++ kernel/device/character.cpp
@@ -0,0 +1,14 @@
+#include "character.hpp"
+
+namespace device {
+
+int character::read(char *, size_t) {
+    return -1;
+}
+
+int character::write(const char *, size_t) {
+    return -1;
+}
+
+} // namespace device
+
--- kernel/device/character.hpp
+++ kernel/device/character.hpp
@@ -11,13 +11,8 @@ struct character {
     virtual ~character() = default;
     virtual const char *name() = 0;
 
-    virtual int read(char *, size_t) {
-        return -1;
-    }
-
-    virtual int write(const char *, size_t) {
-        return -1;
-    }
+    virtual int read(char *, size_t);
+    virtual int write(const char *, size_t);
 
 private:
     utils::kernel_list<character>::node node_;
