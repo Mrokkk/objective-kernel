b82d8e906714e6d2f3bc4d5ca67934c7916019f4
Mrokkk <mrokk@protonmail.com>
2017-06-07 09:49:49 +0200

    Minor changes in scheduler

--- kernel/scheduler/scheduler.cpp
+++ kernel/scheduler/scheduler.cpp
@@ -6,26 +6,21 @@ namespace scheduler {
 
 scheduler *scheduler::instance_;
 
+scheduler::scheduler() : kernel::component(name_), run_queue_(&process::running) {
+    instance_ = this;
+}
+
+void scheduler::initialize() {
+    init_process_.context.esp0 = reinterpret_cast<uint32_t>(init_process_stack_) + init_stack_size_;
+    cpu::gdt::set_tss(init_process_.context);
+}
+
 void scheduler::schedule() {
     auto last = current_process_;
     if (run_queue_.empty()) {
         current_process_ = &init_process_;
     }
     cpu::context_switch(last->context, current_process_->context);
-    ++context_switches_;
-}
-
-scheduler::scheduler() : kernel::component(name_), run_queue_(&process::running) {
-    instance_ = this;
-}
-
-void scheduler::initialize() {
-    init_process_.context.esp0 = reinterpret_cast<uint32_t>(init_process_stack_) + 2048;
-    cpu::gdt::set_tss(init_process_.context);
-}
-
-scheduler &scheduler::instance() {
-    return *instance_;
 }
 
 void scheduler::clone_process() {
@@ -35,6 +30,10 @@ void scheduler::clone_process() {
     new_process->ppid = current_process_->pid;
     processes_.push_back(new_process);
     run_queue_.push_back(*new_process);
+}
+
+scheduler &scheduler::instance() {
+    return *instance_;
 }
 
 asmlinkage void do_scheduler() {
--- kernel/scheduler/scheduler.hpp
+++ kernel/scheduler/scheduler.hpp
@@ -12,14 +12,14 @@ namespace scheduler {
 class scheduler : public kernel::component {
 
     constexpr static const char *name_ = "scheduler";
+    constexpr static const size_t init_stack_size_ = 2048;
     static scheduler *instance_;
 
     utils::list<utils::shared_ptr<process>> processes_;
     utils::kernel_list<process> run_queue_;
-    uint8_t init_process_stack_[2048];
+    uint8_t init_process_stack_[init_stack_size_];
     process init_process_;
     process *current_process_ = &init_process_;
-    size_t context_switches_;
     pid_t current_pid_;
 
 public:
